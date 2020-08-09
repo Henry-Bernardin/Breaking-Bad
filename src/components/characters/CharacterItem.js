c1a61da202443751e0294cab16755e7bd0fb3876
Henry-Bernardin <henry.bernardin@gmail.com>
2020-08-09 13:26:33 -0400

    Characteritem component completed

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -5,9 +5,9 @@ import React from 'react'
         <div className='card'>
         <div className='card-inner'>
           <div className='card-front'>
-            <img src={item.img} alt='' />
+            <img src={item.img} alt='' /> // On the front of the card we have the image 
           </div>
-          <div className='card-back'>
+          <div className='card-back'> // On the card back with item.name and a ul of their info
             <h1>{item.name}</h1>
             <ul>
               <li>
