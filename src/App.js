47c60f483527b3e2531088124106911c4f6ff83b
Henry-Bernardin <henry.bernardin@gmail.com>
2020-08-13 21:39:35 -0400

    getQuery

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ fetchedItems()
   return (
   <div className="container">
     <Header />
-    <Search getQuery={} />
+    <Search getQuery={(q) => setQuery(q)} />
     <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   )
