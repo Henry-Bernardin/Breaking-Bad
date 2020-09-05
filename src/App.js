d91addd25dfbaf37270fa3d12f498a28b7b1ea40
Henry-Bernardin <henry.bernardin@gmail.com>
2020-09-04 22:56:12 -0400

    comment

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const result = await axios(
   `https://www.breakingbadapi.com/api/characters?name=${query}`
   )
 
-  // console.log(result.data)
+  //console.log(result.data)
   
   setItems(result.data)
   setIsLoading(false)
